d5533ebb0288877a67fbc61e1f010f2541a8ba4e
sarori <salo3678@gmail.com>
2021-05-22 00:10:41 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import React from "react"
 import styled, { ThemeProvider } from "styled-components"
 import { ourTheme } from "./styles"
 
-const Container = styled.div`
+interface IContainerProps {
+	floating: boolean
+}
+
+const Container = styled.div<IContainerProps>`
 	background-color: ${(props) => props.theme.bgColor};
 	color: ${(props) => props.theme.fontColor};
+	box-shadow: ${(props) => (props.floating ? "" : "")};
 `
 
 function App() {
 	return (
 		<ThemeProvider theme={ourTheme}>
-			<Container>App</Container>
+			<Container floating={true}>App</Container>
 		</ThemeProvider>
 	)
 }
