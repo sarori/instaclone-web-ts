74f487901f1bda72ddb10ab283478bc9dcb2b0c0
sarori <salo3678@gmail.com>
2021-05-21 23:58:33 +0200

    styled components ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styled, { ThemeProvider } from "styled-components"
+import { ourTheme } from "./styles"
+
+const Container = styled.div`
+	background-color: ${(props) => props.theme.bgColor};
+	color: ${(props) => props.theme.fontColor};
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={ourTheme}>
+			<Container>App</Container>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
